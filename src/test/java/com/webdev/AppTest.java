0e6a0aec1f9f6de6f92abbc01ccfe713c89bb6b0
seaboyz <seaboyz@hotmail.com>
2022-05-15 13:16:56 -0500

    Add Junit and mockito dependency

--- src/test/java/com/webdev/AppTest.java
+++ src/test/java/com/webdev/AppTest.java
@@ -1,20 +1,38 @@
 package com.webdev;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
-import org.junit.Test;
+public class StandardTests {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+  @BeforeAll
+  public static void init() throws Exception {
+  }
+
+  @BeforeEach
+  public void setUp() throws Exception {
+  }
+
+  @AfterEach
+  public void tearDown() throws Exception {
+  }
+
+  @AfterAll
+  void destory() {
+    assert (true);
+  }
+
+  // @Test
+  // public void test() {
+  // fail("Not yet implemented");
+  // }
+
+  // @Test
+  // @Disabled("for demonstration purposes")
+  // void skippedTest() {
+  // // not executed
+  // }
+
 }
